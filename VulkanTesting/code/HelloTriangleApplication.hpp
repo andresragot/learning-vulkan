1a34669a4761d2a1dd1d31e03199d15390bae90e
Andrés Ragot <andresragot99@gmail.com>
2025-10-16 21:25:37 +0200

    window creation

--- VulkanTesting/code/HelloTriangleApplication.hpp
+++ VulkanTesting/code/HelloTriangleApplication.hpp
@@ -7,34 +7,69 @@
 
 #pragma once
 
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 namespace  Ragot
 {
     class HelloTriangleApplication
     {
+    private:
+        static constexpr unsigned WIDTH = 800;
+        static constexpr unsigned HEIGHT = 600;
+    
+        GLFWwindow * window;
+        VkInstance vk_instance;
+    
     public:
         void run ()
         {
+            initWindow();
             initVulkan ();
             mainLoop ();
             cleanup ();
         }
         
     private:
+        void initWindow()
+        {
+            glfwInit();
+            
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+            
+            window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        }
+    
         void initVulkan()
         {
-        
+            createInstance();
         }
         
         void mainLoop()
         {
-        
+            while (not glfwWindowShouldClose(window))
+            {
+                glfwPollEvents();
+            }
         }
         
         void cleanup()
         {
+            glfwDestroyWindow(window);
+            
+            glfwTerminate();
+        }
         
+        void createInstance()
+        {
+            VkApplicationInfo app_info {};
+            app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+            app_info.pApplicationName = "Hello Triangle";
+            app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            app_info.pEngineName = "No Engine";
+            app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            app_info.apiVersion = VK_API_VERSION_1_0;
         }
     };
 }
